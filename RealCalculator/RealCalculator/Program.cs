45a94aac3afd6a8555106b7f039c3592ea16048a
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2023-03-07 09:40:10 +0100

    Add files via upload

---
+++ RealCalculator/RealCalculator/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace RealCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");   
+            string inputOne = Console.ReadLine();
+
+
+            Console.WriteLine("Enter one more number");
+            string inputTwo = Console.ReadLine();
+
+            int numberParsedOne;
+            int numberParsedTwo;
+
+            bool parsedAnswerOne = int.TryParse(inputOne, out numberParsedOne);
+            bool parsedAnswerTwo = int.TryParse(inputTwo, out numberParsedTwo);
+
+            if (!parsedAnswerOne || !parsedAnswerTwo)
+            {
+                Console.WriteLine("You need to put a valid number");
+                return;
+            }
+            
+
+            Console.WriteLine("What operation would you like to do: +, - , * , /,");
+            string operation = Console.ReadLine();
+
+            if(operation == "/")
+            {
+                Console.WriteLine($"The result is: {numberParsedOne / numberParsedTwo}");
+            }
+            else if(operation == "+")
+            {
+                Console.WriteLine($"The result is: {numberParsedOne + numberParsedTwo}");
+            } 
+            else if(operation == "-")
+            {
+                Console.WriteLine($"The result is: {numberParsedOne - numberParsedTwo}");
+            } 
+            else if(operation == "*")
+            {
+                Console.WriteLine($"The result is: {numberParsedOne * numberParsedTwo}");
+            }
+            else
+            {
+                Console.WriteLine("You need to put a valid operation");
+            }
+        }
+    }
+}
